4d558b73c345bbdb234a29b0c6b675de72cfe61c
Anak Agung Gede Agung Davin <anakagung02@mail.ugm.ac.id>
2022-09-27 11:38:13 +0700

    Initial Commit

---
+++ Heal/HealApp.swift
@@ -0,0 +1,20 @@
+//
+//  HealApp.swift
+//  Heal
+//
+//  Created by Anak Agung Gede Agung Davin on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HealApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
