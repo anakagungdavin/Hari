ecb02fa9a1abb041791e1360fa07597df19e7938
herih13 <heri.hermawan13@gmail.com>
2022-11-10 17:49:47 +0700

    Added Dates, symbols and chart

--- Heal/Features/Dashboard/View/DashboardView.swift
+++ Heal/Features/Dashboard/View/DashboardView.swift
@@ -14,8 +14,11 @@ struct DashboardView: View {
     @StateObject var dashboardViewModel = DashboardViewModel()
     @State var isPresented = false
     
-    let weekdays = Calendar.current.shortWeekdaySymbols
-    let ecg = [66, 60, 70, 85, 90, 100, 130]
+//    let weekdays = Calendar.current.shortWeekdaySymbols
+//    let ecg = [66, 60, 70, 85, 90, 100, 130]
+    
+    let weekdays = [0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+    let ecg = [66, 60, 70, 85, 90, 100, 130,140,150,160,170,180,190,200,210,220,230,240]
     
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Ecg.timeStampECG, ascending: true)],
@@ -31,58 +34,148 @@ struct DashboardView: View {
     
     var body: some View {
         GeometryReader{ geometry in
+            Image("kotak dashboard atas")
+                .ignoresSafeArea()
+            
             ScrollView{
                 ZStack {
-                    Group{
-                        HStack{
-                            Image("Group 34")
-                                .padding(EdgeInsets(top: 32, leading: -39, bottom: 0, trailing: 0))
-                            
-                            VStack(alignment: .leading){
-                                Text("Halo!")
-                                    .font(.custom("SFProRounded-Semibold", size: 38))
-                                    .foregroundColor(Color(hex: "B2444E"))
-                                Text("Selamat datang Oktober")
-                                    .font(.custom("SFProRounded-Light", size: 22))
-                                    .foregroundColor(Color(hex: "B2444E"))
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            } //vstack
-                            .padding(EdgeInsets(top: 45, leading: 0, bottom: 0, trailing: 0))
-                        } //hstack
-                        .frame(maxWidth: .infinity)
+                    Image("Group 34")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(EdgeInsets(top: 32, leading: -39, bottom: 0, trailing: 0))
+                    
+                    VStack(alignment: .leading){
+                        Text("Halo!")
+                            .font(.custom("SFProRounded-Semibold", size: 38))
+                            .foregroundColor(Color(hex: "B2444E"))
+                        Text("Selamat datang Oktober")
+                            .font(.custom("SFProRounded-Light", size: 22))
+                            .foregroundColor(Color(hex: "B2444E"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    } //vstack
+                    .padding(EdgeInsets(top: -90, leading: 170, bottom: 0, trailing: 0))
+                    
+                    HStack{
+                        ForEach(0..<3, id: \.self){ i in
+                            ZStack{
+                                Image("card tanggal")
+                                    .opacity(i == 0 ? 1 : 0)
+                                
+                                VStack{
+                                    Text(DateFormatter.displayMonth.string(from: Calendar.current.date(byAdding: .day, value: i, to: Date())!))
+                                        .font(.custom("SFProRounded-Light", size: 20))
+                                        .foregroundColor(Color(hex: "B2444E"))
+                                    Text(DateFormatter.displayDate.string(from: Calendar.current.date(byAdding: .day, value: i, to: Date())!))
+                                        .foregroundColor(Color(hex: "B2444E"))
+                                        .font(.custom("SFProRounded-Semibold", size: 20))
+                                }
+                            }
+                        }
                     }
+                    .padding(EdgeInsets(top: 130, leading: 130, bottom: 0, trailing: 0))
                 } //zstack
                 
-                Group{
-                    VStack{
-                        Text("Lengkapi Jurnalmu")
-                            .foregroundColor(.white)
-                            .font(.custom("SF Pro Rounded", size: 20))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 13)
-                            .padding(.top, 11)
-                            .padding(.bottom, 19)
-                        HStack{
-                            Circle()
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.white)
-                                .padding(.leading, 38)
-                            Circle()
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.white)
-                                .padding(.leading, 67)
-                            Circle()
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.white)
-                                .padding(.leading, 67)
-                                .padding(.trailing, 38)
+                VStack{
+                    Group{
+                        ZStack {
+                            Image("box blom lengkap")
+                            
+                            VStack{
+                                Text("Lengkapi Jurnalmu")
+                                    .foregroundColor(.white)
+                                    .font(.custom("SFProRounded-Semibold", size: 20))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.leading, 13)
+                                    .padding(.top, 11)
+                                    .padding(.bottom, 19)
+                                HStack{
+                                    Image("bpm aman")
+                                        .padding(.leading, 65)
+                                    Image("ecg kosong")
+                                        .padding(.leading, 67)
+                                    Image("aktivitas kosong")
+                                        .padding(.leading, 67)
+                                        .padding(.trailing, 67)
+                                }
+                                .padding(.bottom, 46)
+                            } //VStack
+                            .frame(width: 330, height: 130)
+                        } //Zstack
+                    }.padding(EdgeInsets(top: 33, leading: 27, bottom: 0, trailing: 30))
+                    
+                    Group{
+                        VStack{
+                            Text("Rangkuman Bulan Ini")
+                                .foregroundColor(Color(hex: "B2444E"))
+                                .font(.custom("SFProRounded-Regular", size: 22))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            HStack {
+                                ScrollView(.horizontal, showsIndicators: false){
+                                    Chart{
+                                        ForEach(weekdays, id: \.self){ index in
+                                            PointMark(x: .value("Day", weekdays[index]), y: .value("ECG", ecg[index]))
+                                        }
+                                    }
+                                    .frame(width: CGFloat(ecg.count) * 50)
+                                    .scaledToFit()
+                                } //scrollview
+                                .frame(width: 248, height: 155)
+                                
+                                VStack{
+                                    VStack(alignment: .center){
+                                        Text("\(Int(10)) / \(Int(14))")
+                                            .font(.custom("SFProRounded-Semibold", size: 20))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                        Text("Jurnal")
+                                            .font(.custom("SFProRounded-Light", size: 12))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                        Text("Lengkap")
+                                            .font(.custom("SFProRounded-Light", size: 12))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                        ProgressView(value: 10, total: 14)
+                                            .tint(Color(hex: "F27D87"))
+                                            .background(Color(hex: "FFCED2"))
+                                            .padding(EdgeInsets(top: 0, leading: 9, bottom: 0, trailing: 9))
+                                            .scaleEffect(x: 1, y: 1.5, anchor: .center)
+                                    }.frame(width: 90, height: 76)
+                                        .overlay{
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .stroke(Color(hex: "FFCED2"))
+                                        }
+                                        .padding(.trailing, 32)
+                                    
+                                    VStack(alignment: .center){
+                                        Text("8")
+                                            .font(.custom("SFProRounded-Semibold", size: 20))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                        Text("Gejala")
+                                            .font(.custom("SFProRounded-Light", size: 12))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                        Text("terdeteksi")
+                                            .font(.custom("SFProRounded-Light", size: 12))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                        Text("bulan ini")
+                                            .font(.custom("SFProRounded-Light", size: 12))
+                                            .foregroundColor(Color(hex: "B2444E"))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                    }.frame(width: 90, height: 76)
+                                        .overlay{
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .stroke(Color(hex: "FFCED2"))
+                                        }
+                                        .padding(.trailing, 32)
+                                } //VStack
+                            }
                         }
-                        .padding(.bottom, 46)
                     }
-                    .frame(width: 330, height: 130)
-                    .background(Color(hex: "F27D87")).cornerRadius(10)
-                }.padding(EdgeInsets(top: 33, leading: 27, bottom: 0, trailing: 0))
-                
+                    .padding(EdgeInsets(top: 17, leading: 28, bottom: 0, trailing: 0))
+                }
             }//ScrollView
         }
         .background(Color(hex: "FFFFFF"))
@@ -110,4 +203,18 @@ struct Dashboard_Previews: PreviewProvider {
     static var previews: some View {
         DashboardView()
     }
+}
+
+extension DateFormatter {
+    static let displayDate: DateFormatter = {
+         let formatter = DateFormatter()
+         formatter.dateFormat = "dd"
+         return formatter
+    }()
+    
+    static let displayMonth: DateFormatter = {
+         let formatter = DateFormatter()
+         formatter.dateFormat = "E"
+         return formatter
+    }()
 }
