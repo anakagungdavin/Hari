089158c31603a8851eb460d314c93d45bcfa88e3
herih13 <heri.hermawan13@gmail.com>
2022-11-15 23:25:33 +0700

    removed unnecessary codes

--- Heal/Features/Dashboard/View/DashboardView.swift
+++ Heal/Features/Dashboard/View/DashboardView.swift
@@ -386,22 +386,6 @@ struct DashboardView: View {
     }
 }
 
-struct RoundedRectProgressViewStyle: ProgressViewStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        ZStack(alignment: .leading) {
-            RoundedRectangle(cornerRadius: 14)
-                .frame(width: 250, height: 28)
-                .foregroundColor(.blue)
-                .overlay(Color.black.opacity(0.5)).cornerRadius(14)
-            
-            RoundedRectangle(cornerRadius: 14)
-                .frame(width: CGFloat(configuration.fractionCompleted ?? 0) * 250, height: 28)
-                .foregroundColor(.yellow)
-        }
-        .padding()
-    }
-}
-
 struct Dashboard_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack{
